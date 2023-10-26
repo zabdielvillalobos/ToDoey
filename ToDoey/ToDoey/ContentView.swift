e9ec60cc46d1dbb9b8dfb4cc9a706947be5a7668
Zabdiel Villalobos <zabdielv@ad.unc.edu>
2023-10-26 23:05:20 +0000

    Setting up GitHub Classroom Feedback

---
+++ ToDoey/ToDoey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoey
+//
+//  Created by Zabdiel Villalobos on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoey/ToDoey/ToDoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoeyApp.swift
+//  ToDoey
+//
+//  Created by Zabdiel Villalobos on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
